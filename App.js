e0e4c11da6ffff823e1d1fd66187cdae8f154e2b
Siya <siyanda.makhalima@sovtech.com>
2022-04-18 14:53:18 +0200

    added maps library and view the map, WIP

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Siya</Text>
+      <MapView 
+        style={styles.map}
+        coordinate={{ latitude : -34.02516803331711 , longitude : 18.670542535167005 }}
+       />
     </View>
+      
   );
 }
 
@@ -17,4 +22,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  }
 });
